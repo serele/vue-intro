0fee9675158c5da249137bc036dc62886bcf651f
serele <sergio.srl@gmail.com>
2019-09-21 22:37:59 +0100

    List Rendering

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ var app = new Vue({
         description: 'Pair of short socks',
         image: './assets/vmSocks-green.jpg',
         inventory: 8,
-        onSale: false
+        onSale: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["S", "M", "L", "XL"]
     }
 })
