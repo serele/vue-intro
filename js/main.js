9ec6d347036280d351f0ba2f458c8dea19019908
serele <sergio.srl@gmail.com>
2019-09-21 21:40:38 +0100

    Attribute Binding

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'Pair of short socks'
+        description: 'Pair of short socks',
+        image: './assets/vmSocks-green.jpg'
     }
 })
