ef3536fe650f35166c01a33a969acb32dbf82551
serele <sergio.srl@gmail.com>
2019-09-30 12:50:51 +0100

    Computed Properties

--- js/main.js
+++ js/main.js
@@ -3,22 +3,25 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         description: 'Pair of short socks',
-        image: './assets/vmSocks-green.jpg',
-        inventory: 8,
-        onSale: false,
+        selectedVariant: 0,                
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './assets/vmSocks-green.jpg'
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10,
+                onSale: true
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0,
+                onSale: false
             }
         ],
         sizes: ["S", "M", "L", "XL"],
@@ -26,21 +29,41 @@ var app = new Vue({
     },
     methods: {
         addToCart: function () {
-            if (this.inventory >= 1) {
-                this.cart += 1
-                this.inventory -=1
+            if (this.inStock >= 1) {
+                this.cart += 1                
+                this.variants[this.selectedVariant].variantQuantity -=1
             }            
         },
 
         removeFromCart: function () {
             if (this.cart > 0) {
                 this.cart -= 1
-                this.inventory +=1
+                this.variants[this.selectedVariant].variantQuantity +=1
             }
         },
 
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        almostSoldOut() {
+            if (this.variants[this.selectedVariant].variantQuantity <= 10)
+                return true
+        },
+        onSale() {
+            if (this.variants[this.selectedVariant].onSale)
+                return true
         }
     }
 })
